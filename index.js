32ff9c55f4b1f0db030cd1bfb1f6bdbcef69aa0e
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-01-11 00:08:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const { Pool } = require("pg");
 
 const app = express();
 app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
 
 app.get("/", (req, res) => {
   res.send("AutoDriv Core OK");
 });
 
-app.post("/webhook", (req, res) => {
+app.post("/webhook", async (req, res) => {
   res.sendStatus(200);
 });
 

fe38958fd0447376315873ba4657bfaba7faeba0
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-16 19:58:34 -0300

    Update vehicles.service.js

--- src/modules/vehicles/vehicles.service.js
+++ src/modules/vehicles/vehicles.service.js
@@ -1,13 +1,34 @@
+const pool = require("../../config/db");
 const repo = require("./vehicles.repository");
+const {
+  buildVehicleSlug,
+  buildSeoTitle,
+  buildSeoDescription
+} = require("../../utils/seo");
 
 async function createVehicle(data, user) {
+  const dealershipResult = await pool.query(
+    `SELECT * FROM dealerships WHERE id = $1`,
+    [user.dealershipId]
+  );
+
+  const dealership = dealershipResult.rows[0];
+
+  const slug = buildVehicleSlug(data);
+
+  const seoTitle = buildSeoTitle(data, dealership);
+  const seoDescription = buildSeoDescription(data, dealership);
+
   return repo.create({
     dealership_id: user.dealershipId,
     title: data.title,
     brand: data.brand,
     model: data.model,
     year: data.year,
-    price: data.price
+    price: data.price,
+    slug,
+    seo_title: seoTitle,
+    seo_description: seoDescription
   });
 }
 

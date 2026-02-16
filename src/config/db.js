5e0d698b6e0b55bd6deca1706c31f1ff2818c3d9
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-16 14:46:31 -0300

    Create db.js

---
+++ src/config/db.js
@@ -0,0 +1,9 @@
+const { Pool } = require("pg");
+const { DATABASE_URL } = require("./env");
+
+const pool = new Pool({
+  connectionString: DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+module.exports = pool;

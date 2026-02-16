5e66bdae1e47355dd4a84bd7dfde4329a731d77e
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-16 16:23:09 -0300

    Create maintenance.routes.js

---
+++ src/modules/maintenance/maintenance.routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const controller = require("./maintenance.controller");
+const auth = require("../../middlewares/auth.middleware");
+
+const router = express.Router();
+
+router.post("/", auth, controller.create);
+router.get("/vehicle/:vehicleId", auth, controller.getByVehicle);
+router.put("/task/:taskId", auth, controller.updateTask);
+router.put("/vehicle/:vehicleId/documentation", auth, controller.updateDocumentation);
+
+module.exports = router;

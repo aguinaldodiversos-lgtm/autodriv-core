d77df06b57bbaefdc0318d17700136e3b988627b
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-16 14:47:45 -0300

    Create plans.js

---
+++ src/config/plans.js
@@ -0,0 +1,11 @@
+module.exports = {
+  starter: {
+    limits: { vehicles: 10, users: 1 }
+  },
+  pro: {
+    limits: { vehicles: 30, users: 3 }
+  },
+  master: {
+    limits: { vehicles: "unlimited", users: "unlimited" }
+  }
+};

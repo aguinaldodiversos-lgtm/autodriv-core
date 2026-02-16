f9bc9b661e9ab803f5eea9d7efc0b52d5dea194d
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-16 15:10:37 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,22 @@ const express = require("express");
 const authRoutes = require("./modules/auth/auth.routes");
 
 const app = express();
+const express = require("express");
+const authRoutes = require("./modules/auth/auth.routes");
+const vehicleRoutes = require("./modules/vehicles/vehicles.routes");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("AutoDriv Core API");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/vehicles", vehicleRoutes);
+
+module.exports = app;
+
 app.use(express.json());
 
 app.get("/", (req, res) => {

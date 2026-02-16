c6cd47ca7702e6c11f8c15947635057b8d0751b3
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-16 15:47:16 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const authRoutes = require("./modules/auth/auth.routes");
 const vehicleRoutes = require("./modules/vehicles/vehicles.routes");
 const clientRoutes = require("./modules/clients/clients.routes");
 const leadRoutes = require("./modules/leads/leads.routes");
+const proposalRoutes = require("./modules/proposals/proposals.routes");
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/vehicles", vehicleRoutes);
 app.use("/api/clients", clientRoutes);
 app.use("/api/leads", leadRoutes);
+app.use("/api/proposals", proposalRoutes);
 
 /* =========================
    ROTA NÃO ENCONTRADA

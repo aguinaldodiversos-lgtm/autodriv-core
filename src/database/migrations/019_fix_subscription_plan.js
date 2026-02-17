ceb5f1ec53a8ae817780919b136cd8435c07204c
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-17 08:11:11 -0300

    Update 019_fix_subscription_plan.js

--- src/database/migrations/019_fix_subscription_plan.js
+++ src/database/migrations/019_fix_subscription_plan.js
@@ -2,13 +2,11 @@ module.exports = {
   name: "019_fix_subscription_plan",
 
   async up(pool) {
-    // remove constraint antiga
     await pool.query(`
       ALTER TABLE subscriptions
       DROP CONSTRAINT IF EXISTS subscriptions_plan_check;
     `);
 
-    // cria nova constraint com trial
     await pool.query(`
       ALTER TABLE subscriptions
       ADD CONSTRAINT subscriptions_plan_check

8dd18f7bd8aeeff8bba0cc0a0ac2cf2b3a78ef37
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-16 21:30:47 -0300

    Create ads.routes.js

---
+++ src/modules/ads/ads.routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const controller = require("./ads.controller");
+const auth = require("../../middlewares/auth.middleware");
+
+const router = express.Router();
+
+router.post("/generate", auth, controller.generate);
+router.get("/vehicle/:vehicleId", auth, controller.list);
+
+module.exports = router;

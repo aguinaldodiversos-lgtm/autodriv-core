6e66f5a98d15a5fb5f01d698d41d5c629222b595
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-17 00:30:16 -0300

    Update dashboard.routes.js

--- src/modules/dashboard/dashboard.routes.js
+++ src/modules/dashboard/dashboard.routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const controller = require("./dashboard.controller");
-const auth = require("../../middlewares/auth.middleware");
+const auth = require("../../middlewares/auth");
 
 const router = express.Router();
 
-router.get("/alerts", auth, controller.alerts);
+router.get("/alerts", auth, controller.getAlerts);
 
 module.exports = router;

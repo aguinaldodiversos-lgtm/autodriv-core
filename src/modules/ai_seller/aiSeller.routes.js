bb17722818c75abddcea339d08053b8cc89f8781
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-17 14:58:09 -0300

    Update aiSeller.routes.js

--- src/modules/ai_seller/aiSeller.routes.js
+++ src/modules/ai_seller/aiSeller.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const controller = require("./aiSeller.controller");
+const auth = require("../../middlewares/auth.middleware");
 
 const router = express.Router();
 
-router.post("/message", controller.message);
+router.post("/message", auth, controller.message);
 
 module.exports = router;

306a8f2524cd0fb5f134b73bc327db449ca2305e
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-17 22:39:35 -0300

    Update leads.routes.js

--- src/modules/leads/leads.routes.js
+++ src/modules/leads/leads.routes.js
@@ -5,6 +5,7 @@ const auth = require("../../middlewares/auth.middleware");
 const router = express.Router();
 
 router.get("/", auth, controller.list);
+router.post("/:id/reactivate", auth, controller.reactivate);
 router.post("/", auth, controller.create);
 router.put("/:id", auth, controller.update);
 router.delete("/:id", auth, controller.remove);

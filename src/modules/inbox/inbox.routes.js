5e4d8c43de16016b79e3e0a18d6932027d0faace
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-18 07:48:55 -0300

    Create inbox.routes.js

---
+++ src/modules/inbox/inbox.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const controller = require("./inbox.controller");
+const auth = require("../../middlewares/auth.middleware");
+
+const router = express.Router();
+
+router.get("/", auth, controller.list);
+router.get("/:leadId", auth, controller.get);
+router.post("/:leadId/send", auth, controller.send);
+
+module.exports = router;

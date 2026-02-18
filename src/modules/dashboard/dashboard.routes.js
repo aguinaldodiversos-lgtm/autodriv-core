e7e0b95bf23dbdb3263e039fcdf43e27d9cd1c9b
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-17 23:31:59 -0300

    Update dashboard.routes.js

--- src/modules/dashboard/dashboard.routes.js
+++ src/modules/dashboard/dashboard.routes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get("/", auth, controller.getStats);
 router.get("/recovery", auth, controller.recoveryStats);
 router.get("/alerts", auth, controller.alerts);
+router.get("/forecast", auth, controller.forecast);
 
 module.exports = router;

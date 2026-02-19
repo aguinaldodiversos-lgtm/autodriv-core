989c713212cd20a989640e001f998d6677653cfa
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-18 22:06:25 -0300

    Update forecast.routes.js

--- src/modules/analytics/forecast.routes.js
+++ src/modules/analytics/forecast.routes.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const controller = require("./forecast.controller");
+const auth = require("../../middlewares/auth.middleware");
 
+const router = express.Router();
+
+router.get("/monthly", auth, controller.getMonthlyForecast);
+
+module.exports = router;

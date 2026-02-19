7050a29dc43fee81d523cbae51f052eea27c21b1
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-18 22:40:30 -0300

    Create notification.routes.js

---
+++ src/modules/notifications/rules/notification.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const controller = require("./notification.controller");
+const auth = require("../../middlewares/auth.middleware");
+
+const router = express.Router();
+
+router.get("/", auth, controller.getNotifications);
+
+module.exports = router;

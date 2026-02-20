689ab0aecc5298e321a0a141906debea97adb72b
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-19 21:51:00 -0300

    Update contracts.routes.js

--- src/modules/contracts/contracts.routes.js
+++ src/modules/contracts/contracts.routes.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const controller = require("./contracts.controller");
 const authMiddleware = require("../../middlewares/auth.middleware");
 
+router.post("/:id/send-approval", authMiddleware, controller.sendForApproval);
+router.post("/:id/approve", authMiddleware, controller.approve);
+router.post("/:id/reject", authMiddleware, controller.reject);
 router.post("/:saleId/generate", authMiddleware, controller.generate);
 
 module.exports = router;

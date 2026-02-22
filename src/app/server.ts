c36f831037658bd55b1f7ba8d9f3ea95d62914d1
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-21 22:09:44 -0300

    Update server.ts

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import { bootstrap } from "./bootstrap"
-
+import { systemRoutes } from "@/interfaces/http/routes/system.routes"
 dotenv.config()
 
 async function start() {
@@ -10,7 +10,7 @@ async function start() {
   app.use(express.json())
 
   const context = await bootstrap(process.env)
-
+  app.use("/api/system", systemRoutes(context))
   app.get("/health", (_, res) => {
     res.json({ status: "AIP running" })
   })

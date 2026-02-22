9449fbe1163fe4ef8440e3d4293ba5f37698f04b
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-21 21:00:54 -0300

    Update event.bus.ts

--- src/infrastructure/event-bus/event.bus.ts
+++ src/infrastructure/event-bus/event.bus.ts
@@ -1,20 +1,26 @@
 import { DomainEvent } from "./event.types"
 import { EventHandler } from "./event.handler"
+import PQueue from "p-queue"
 
 export class EventBus {
+
   private handlers: EventHandler[] = []
+  private queue = new PQueue({ concurrency: 10 })
 
   register(handler: EventHandler) {
     this.handlers.push(handler)
   }
 
   async publish(event: DomainEvent) {
+
     const matching = this.handlers.filter(
       h => h.eventName === event.name
     )
 
     await Promise.all(
-      matching.map(h => h.handle(event))
+      matching.map(handler =>
+        this.queue.add(() => handler.handle(event))
+      )
     )
   }
 }
